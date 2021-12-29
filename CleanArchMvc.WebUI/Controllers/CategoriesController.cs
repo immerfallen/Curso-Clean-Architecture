48c5b811311b1c0e4355bfff3cc67c12192b9dc9
immerfallen <marodemelo10@gmail.com>
2021-12-29 15:39:57 -0300

    criado front para tesetes de categoria

---
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        private readonly ICategoryService _catService;
+
+        public CategoriesController(ICategoryService catService)
+        {
+            _catService = catService;
+        }
+
+        [HttpGet]
+        public async  Task<IActionResult> Index()
+        {
+            var categories = await _catService.GetCategories();
+            return View(categories);
+        }
+    }
+}
---
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private readonly IProductService _prodService;
+
+        public ProductsController(IProductService prodService)
+        {
+            _prodService = prodService;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            var products = await _prodService.GetProducts();
+            return View(products);
+        }
+    }
+}
