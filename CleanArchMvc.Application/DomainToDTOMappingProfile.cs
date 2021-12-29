d465b090d18979f79eb9f4d205b4d2dd6dd5e755
immerfallen <marodemelo10@gmail.com>
2021-12-29 12:04:36 -0300

    Iniciando a criacao do mapeamento

---
+++ CleanArchMvc.Application/DomainToDTOMappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using CleanArchMvc.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application
+{
+    class DomainToDTOMappingProfile : Profile
+    {
+        public DomainToDTOMappingProfile()
+        {
+            CreateMap<Product, ProductDTO>().ReverseMap();
+            CreateMap<Category, CategoryDTO>().ReverseMap();
+        }
+    }
+}
