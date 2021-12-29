73b85d363c2cb8a199776223256b4e855a5c9db0
Maro de Melo <maromelo@tempus.digital>
2021-12-29 10:35:49 -0300

    Implementacao dos repositorios

---
+++ CleanArchMvc.Infra.Data/Repositories/CategoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using CleanArchMvc.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    class CategoryRepository : ICategoryRepository
+    {
+        private ApplicationDbContext _categoryContext;
+
+        public CategoryRepository(ApplicationDbContext context)
+        {
+            _categoryContext = context;
+        }
+
+        public async Task<Category> Create(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Category> GetById(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Category>> GetCategories()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Category> Remove(Category category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Category> Update(Category category)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ CleanArchMvc.Infra.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,43 @@
+﻿using CleanArchMvc.Domain.Entities;
+using CleanArchMvc.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.Repositories
+{
+    class ProductRepository : IProductRepository
+    {
+        public async Task<Product> CreateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> GetByIdAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<Product>> GetProductAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> GetProductCategoryAsync(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> RemoveAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<Product> UpdateAsync(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
