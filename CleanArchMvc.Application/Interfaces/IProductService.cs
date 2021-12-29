97c7ec744e1ab43ad7577733f00e6ac69c03eeb9
immerfallen <marodemelo10@gmail.com>
2021-12-29 14:18:47 -0300

    Implementando o servico para product

---
+++ CleanArchMvc.Application/Interfaces/IProductService.cs
@@ -0,0 +1,24 @@
+﻿using CleanArchMvc.Application.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Application.Interfaces
+{
+    interface IProductService
+    {
+        Task<ProductDTO> GetById(int? id);
+
+        Task<IEnumerable<ProductDTO>> GetProducts();
+
+        Task<ProductDTO> GetProductCategory(int? id);
+
+        Task Add(ProductDTO productDto);
+
+        Task Update(ProductDTO productDto);
+
+        Task Remove(int? id);
+    }
+}
