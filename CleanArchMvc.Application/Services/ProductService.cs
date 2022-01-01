bc8bd5f07981384000be18b8e8bdefedb14b994b
immerfallen <marodemelo10@gmail.com>
2022-01-01 13:51:55 -0300

    Criando HomeController, reteamento e views index

--- CleanArchMvc.Application/Services/ProductService.cs
+++ CleanArchMvc.Application/Services/ProductService.cs
@@ -42,16 +42,16 @@ namespace CleanArchMvc.Application.Services
             return _mapper.Map<ProductDTO>(result);
         }
 
-        public async Task<ProductDTO> GetProductCategory(int? id)
-        {
-            var productByIdQuery = new GetProductByIdQuery(id.Value);
-            if (productByIdQuery == null)
-            {
-                throw new Exception($"Entity could not be loaded.");
-            }
-            var result = await _mediator.Send(productByIdQuery);
-            return _mapper.Map<ProductDTO>(result);
-        }
+        //public async Task<ProductDTO> GetProductCategory(int? id)
+        //{
+        //    var productByIdQuery = new GetProductByIdQuery(id.Value);
+        //    if (productByIdQuery == null)
+        //    {
+        //        throw new Exception($"Entity could not be loaded.");
+        //    }
+        //    var result = await _mediator.Send(productByIdQuery);
+        //    return _mapper.Map<ProductDTO>(result);
+        //}
 
         public async Task<IEnumerable<ProductDTO>> GetProducts()
         {
