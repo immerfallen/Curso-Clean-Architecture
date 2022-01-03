933eada6e1df2ad5fbe187f1e7e68873162c1c69
immerfallen <marodemelo10@gmail.com>
2022-01-03 18:50:59 -0300

    Implementada edicao de categorias

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -1,6 +1,7 @@
 ﻿using CleanArchMvc.Application.DTO;
 using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUI.Controllers
@@ -15,17 +16,19 @@ namespace CleanArchMvc.WebUI.Controllers
         }
 
         [HttpGet]
-        public async  Task<IActionResult> Index()
+        public async Task<IActionResult> Index()
         {
             var categories = await _catService.GetCategories();
             return View(categories);
         }
 
-        [HttpGet] 
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
+
+       
 
         [HttpPost]
         public async Task<IActionResult> Create(CategoryDTO category)
@@ -33,6 +36,36 @@ namespace CleanArchMvc.WebUI.Controllers
             if (ModelState.IsValid)
             {
                 await _catService.Add(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
+        }
+
+        [HttpGet()]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null) return NotFound();
+
+            var categoryDto = await _catService.GetById(id);
+            if (categoryDto == null) return NotFound();
+
+            return View(categoryDto);
+        }
+
+        [HttpPost()]
+        public async Task<IActionResult> Edit(CategoryDTO category)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    await _catService.Update(category);
+                }
+                catch (Exception)
+                {
+                    throw;
+                }
+
                 return RedirectToAction(nameof(Index));
             }
             return View(category);
