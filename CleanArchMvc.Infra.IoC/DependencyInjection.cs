0ab96457f05b9bb3179edfa851134ab911b2a13c
immerfallen <marodemelo10@gmail.com>
2022-01-04 20:52:13 -0300

    Incluindo os servicos do Identity e registrando servicos no projeto Infra.Ioc

--- CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -3,10 +3,13 @@
     using CleanArchMvc.Application.Interfaces;
     using CleanArchMvc.Application.Mappings;
     using CleanArchMvc.Application.Services;
+    using CleanArchMvc.Domain.Account;
     using CleanArchMvc.Domain.Interfaces;
     using CleanArchMvc.Infra.Data.Context;
+    using CleanArchMvc.Infra.Data.Identity;
     using CleanArchMvc.Infra.Data.Repositories;
     using MediatR;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
@@ -20,11 +23,23 @@
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
             b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
 
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+               .AddEntityFrameworkStores<ApplicationDbContext>()
+               .AddDefaultTokenProviders();
+
+            services.ConfigureApplicationCookie(options => options.AccessDeniedPath = "/Account/Login");
+
             services.AddScoped<IProductRepository, ProductRepository>();
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductService, ProductService>();
             services.AddScoped<ICategoryService, CategoryService>();
             services.AddAutoMapper(typeof(DomainToDTOMappingProfile));
+
+            services.AddScoped<IAuthenticate, AuthenticateService>();
+            services.AddScoped<ISeedUserRoleInitial, SeedUserRoleInitial>();
+
+
+
 
             var myHandlers = AppDomain.CurrentDomain.Load("CleanArchMvc.Application");
             services.AddMediatR(myHandlers);
