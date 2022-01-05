4333b7046ca3402c2d0a9e495a53c76b529603f9
immerfallen <marodemelo10@gmail.com>
2022-01-05 19:36:34 -0300

    Implementando o login, register e logout em accountcontroller

--- CleanArchMvc.WebUI/Controllers/AccountController.cs
+++ CleanArchMvc.WebUI/Controllers/AccountController.cs
@@ -18,18 +18,50 @@ namespace CleanArchMvc.WebUI.Controllers
         }
 
         [HttpGet]
-        public IActionResult Register() { }
+        public IActionResult Register()
+        {
+            return View();
+        }
 
         [HttpPost]
         public async Task<IActionResult> Register(RegisterViewModel model)
         {
-
+            var result = await _authentication.Authenticate(model.Email, model.Password);
+            if (result) return Redirect("/");
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt. (password must be strong)");
+                return View(model);
+            }
         }
         [HttpGet]
-        public IActionResult Login() { }
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
 
         [HttpPost]
-        public async Task<IActionResult> Login(LoginViewModel model) { }
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            var result = await _authentication.Authenticate(model.Email, model.Password);
+
+            if (result)
+            {
+                if (string.IsNullOrEmpty(model.ReturnUrl))
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+                return Redirect(model.ReturnUrl);
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login attempt. (password must be strong)");
+                return View(model);
+            }
+        }
 
         public async Task<IActionResult> Logout() { }
     }
