0baa9dfb56bc64a76143461310a47e905e05e301
immerfallen <marodemelo10@gmail.com>
2022-01-05 19:26:12 -0300

    Criando o controloador AccountController e as ViewModels na camada WebUI

---
+++ CleanArchMvc.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using CleanArchMvc.Domain.Account;
+using CleanArchMvc.WebUI.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthenticate _authentication;
+
+        public AccountController(IAuthenticate authentication)
+        {
+            _authentication = authentication;
+        }
+
+        [HttpGet]
+        public IActionResult Register() { }
+
+        [HttpPost]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+
+        }
+        [HttpGet]
+        public IActionResult Login() { }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model) { }
+
+        public async Task<IActionResult> Logout() { }
+    }
+}
