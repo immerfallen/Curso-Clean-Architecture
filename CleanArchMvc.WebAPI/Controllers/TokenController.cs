89485372f04caabf9c5caf90e3a6d20413c547e3
immerfallen <marodemelo10@gmail.com>
2022-01-08 12:11:20 -0300

    wip

---
+++ CleanArchMvc.WebAPI/Controllers/TokenController.cs
@@ -0,0 +1,23 @@
+﻿using CleanArchMvc.Domain.Account;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticate _authentication;
+
+        public TokenController(IAuthenticate authentication)
+        {
+            _authentication = authentication ?? 
+                throw new ArgumentNullException(nameof(authentication));
+        }
+    }
+}
