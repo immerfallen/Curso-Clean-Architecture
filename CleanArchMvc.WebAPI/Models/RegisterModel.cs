b892c3206dd204bcfa02860377a650e99a5917a8
immerfallen <marodemelo10@gmail.com>
2022-01-08 11:59:20 -0300

    Configurando as informacaoes do tojen e os DTOs usados

---
+++ CleanArchMvc.WebAPI/Models/RegisterModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.WebAPI.Models
+{
+    public class RegisterModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password", ErrorMessage = "Passwords dont match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
